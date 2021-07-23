be2eeeeb9c17a9480b77c506d73b7f86cdcdb43c
erin <erinverkoulen93@gmail.com>
2021-07-23 12:51:45 -0500

    Crud Lab

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,30 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([]);
+
+  function deleteQuestion(id) {
+    const updatedQuestions = questions.filter(question => question.id !== id)
+    // setQuestions(updatedQuestions);
+  }
+
+  function updateQuestions(id, correctIndex) {
+    const updatedQuestions = questions.map(question => {
+      if(question.id === id) {
+        return [...question, correctIndex]
+      }else {
+        return question;
+      }
+    })
+    setQuestions(updatedQuestions);
+  }
+
+  
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questions={questions} setQuestions={setQuestions} onDeleteQuestion={deleteQuestion} onUpdateQuestion={updateQuestions}/>}
     </main>
   );
 }
